3f93fd897067b1872fb09ae76417343118254ec3
MarcoANT9 <acalde27@hotmail.com>
2020-03-09 22:41:34 +0000

    Task 6: First Commit

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+void print_num(int n);
+
+/**
+ * binary_tree_preorder - Traverses a binary tree using pre-order traversal.
+ *
+ * @tree: Root of the tree.
+ * @func: Function to apply in each node.
+ *
+ * Description - This program traverses a binary tree using the pre-order
+ *               traversal method, this is, starting from the root to the
+ *               leafs.
+ *
+ * Return: Nothing (Void).
+ *
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	func(tree->n);
+
+	if (tree->left != NULL)
+		binary_tree_preorder(tree->left, &print_num);
+
+	if (tree->right != NULL)
+		binary_tree_preorder(tree->right, &print_num);
+
+}
