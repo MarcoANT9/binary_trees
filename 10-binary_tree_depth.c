f164140fedab95301a41a502e7f39c09e4de4486
Noah172 <jsebastian.garcia1995@gmail.com>
2020-03-10 19:56:57 +0000

    Task 10: First Commit

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_depth - function that measures the depth of a node in a binary tree
+ *@tree: pointer to the node to measure the depth
+ *
+ *Return: the depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	while (1)
+	{
+		if (tree->parent == NULL)
+		{
+			return (counter);
+		}
+		else
+		{
+			counter++;
+			tree = tree->parent;
+		}
+	}
+}
