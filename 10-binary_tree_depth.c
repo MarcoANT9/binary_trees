a935a9a51816776bb2db6ee081120a9ed6e89073
Noah172 <jsebastian.garcia1995@gmail.com>
2020-03-10 21:43:32 +0000

    Added: correction of betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- *binary_tree_depth - function that measures the depth of a node in a binary tree
+ *binary_tree_depth - function that measures the depth of a node in a binary
+ *                    tree
  *@tree: pointer to the node to measure the depth
  *
  *Return: the depth of the node
