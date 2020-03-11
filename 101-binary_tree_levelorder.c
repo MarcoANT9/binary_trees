83ca94c675f512061cbddd2d83bfe92ec8214795
MarcoANT9 <acalde27@hotmail.com>
2020-03-11 20:45:37 +0000

    Task 20(101): Modified to include the root

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -18,6 +18,9 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
+	if (tree->parent == NULL)
+		func(tree->n);
+
 	if (tree->left != NULL)
 		func(tree->left->n);
 
