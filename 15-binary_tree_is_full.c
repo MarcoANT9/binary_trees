3224decb601e8272f196a327f18684fc81f0ad34
MarcoANT9 <acalde27@hotmail.com>
2020-03-11 18:53:46 +0000

    Betty correction

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 /**
- *binary_tree_leaves - unction that counts the leaves in a binary tree
- *@tree: pointer to the root node of the tree to count the number of leaves
+ * binary_tree_leaves_int - unction that counts the leaves in a binary tree
+ *
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ *
+ * Description - This function counts all the leaves in a binary tree.
  *
  *Return: the number of leaves or 0 if is NULL
  */
@@ -24,10 +27,12 @@ int binary_tree_leaves_int(const binary_tree_t *tree)
 	return (leaves);
 }
 /**
- *binary_tree_nodes - function that counts the nodes with at least 1 child in a
- *                    binary tree.
+ * binary_tree_nodes_int - function that counts the nodes with at least 1 child
+ *                         in a binary tree.
  *
- *@tree: pointer to the root node of the tree to count the number of nodes
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ *
+ * Description - This function counts all the nodes in a binary tree.
  *
  *Return: The number of nodes with at least 1 child
  */
@@ -50,10 +55,17 @@ int binary_tree_nodes_int(const binary_tree_t *tree)
 	return (size);
 }
 /**
- *binary_tree_is_full - function that checks if a binary tree is full
- *@tree: pointer to the root node of the tree to check
+ * binary_tree_is_full - function that checks if a binary tree is full
  *
- *Return: 0 if the tree is not full
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Description - This function determines if the binary tree is full,
+ *               it does it by counting the number of leaves and nodes
+ *               if the number of nodes is less than the number of leaves,
+ *               the tree is full, otherwise the number of leaves is equal
+ *               to the number of nodes.
+ *
+ * Return: 0 if the tree is not full
  *        1 if the tree is full
  */
 int binary_tree_is_full(const binary_tree_t *tree)
